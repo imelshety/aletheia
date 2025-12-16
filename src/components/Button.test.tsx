b5e7b85c678ab45594ab3bb73dfd34546eb02cbb
Mohamed Elshety <elshetydev99@gmail.com>
2025-12-16 11:31:03 +0200

    fix broken CI workflow in pr

--- src/components/Button.test.tsx
+++ src/components/Button.test.tsx
@@ -1,23 +1,23 @@
-import {render, screen} from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import {describe, it, expect, vi} from 'vitest';
+import { describe, it, expect, vi } from 'vitest';
 import Button from './Button';
 
 describe('Button Component', () => {
-    it('should render the button with the correct label', () => {
-        const handleClick = vi.fn();
-        render(<Button label="Click Me" onClick={handleClick}/>);
-        const buttonElement = screen.getByText(/Click Me/i);
+  it('should render the button with the correct label', () => {
+    const handleClick = vi.fn();
+    render(<Button label='Click Me' onClick={handleClick} />);
+    const buttonElement = screen.getByText(/Click Me/i);
 
-        expect(buttonElement).toBeInTheDocument();
-    });
+    expect(buttonElement).toBeInTheDocument();
+  });
 
-    it('should call the onClick handler when clicked', async () => {
-        const handleClick = vi.fn();
-        render(<Button label="Test Click" onClick={handleClick}/>);
-        const buttonElement = screen.getByText(/Test Click/i);
-        await userEvent.click(buttonElement);
+  it('should call the onClick handler when clicked', async () => {
+    const handleClick = vi.fn();
+    render(<Button label='Test Click' onClick={handleClick} />);
+    const buttonElement = screen.getByText(/Test Click/i);
+    await userEvent.click(buttonElement);
 
-        expect(handleClick).toHaveBeenCalledTimes(1);
-    });
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  });
 });
