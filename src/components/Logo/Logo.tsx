8639f8a580f3724b8864b3171bf2edf60ba51bd9
Mohamed Elshety <elshetydev99@gmail.com>
2025-12-31 02:31:17 +0200

    chore(ds): remove defult value of width and height

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,8 +1,10 @@
 import logo from '@/assets/aletheia.svg';
+
 interface LogoProps {
   width?: number;
   height?: number;
 }
-export function Logo({ width = 42, height = 42 }: LogoProps) {
+
+export function Logo({ width, height }: LogoProps) {
   return <img src={logo} alt='Aletheia Logo' width={width} height={height} />;
 }
