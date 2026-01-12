2b87467fa19133456aab9c7ce9e16a2cf6dee758
Dhemy <imdhemy@gmail.com>
2026-01-12 10:51:26 +0100

    chore(ds): implement secondary button variant (#46)

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,27 +1,44 @@
-import type { ReactNode } from 'react';
+import type { MouseEventHandler, ReactNode } from 'react';
 
 interface ButtonProps {
   disabled?: boolean;
-  children: ReactNode;
-  onClick?: () => void;
+  children?: ReactNode;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
   type?: 'button' | 'submit' | 'reset';
+  variant?: 'primary' | 'secondary';
 }
 
-export function Button({ disabled = false, children, onClick, type = 'button' }: ButtonProps) {
+const commonClasses = `
+  h-10 
+  px-4 
+  py-2 
+  text-sm 
+  inline-flex items-center 
+  justify-center 
+  gap-2 
+  whitespace-nowrap 
+  rounded-md 
+  font-medium 
+  transition-colors
+  w-full
+  focus-visible:outline-none
+  focus-visible:ring-2 
+  focus-visible:ring-offset-2
+  disabled:pointer-events-none
+  disabled:opacity-50 
+  hover:cursor-pointer
+  `;
+
+const variants = {
+  primary: 'bg-brand text-white hover:bg-brand-dark',
+  secondary: 'bg-secondary text-white hover:bg-secondary-dark',
+};
+
+export function Button({ children, onClick, disabled, type = 'button', variant = 'primary' }: ButtonProps) {
+  const classNames = `${commonClasses} ${variants[variant]}`;
+
   return (
-    <button
-      type={type}
-      onClick={disabled ? undefined : onClick}
-      disabled={disabled}
-      className='
-      inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors
-      h-10 px-4 py-2 w-full
-      focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2
-      disabled:pointer-events-none disabled:opacity-50
-      bg-brand text-white
-      hover:cursor-pointer hover:bg-brand-dark
-      '
-    >
+    <button type={type} onClick={onClick} disabled={disabled} className={classNames}>
       {children}
     </button>
   );
