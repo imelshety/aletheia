3561ee39000d271ab3f1f84e1a5c4273af15ab1f
Dhemy <imdhemy@gmail.com>
2026-02-04 20:01:46 +0100

    chore(link): enhance tests for Link component with styled and unstyled props

--- src/components/Link/Link.test.tsx
+++ src/components/Link/Link.test.tsx
@@ -2,40 +2,32 @@ import { MemoryRouter } from '@/components';
 import { render, screen } from '@testing-library/react';
 import { Link } from './Link';
 
-test('it should render Link with correct href attribute', () => {
-  render(
-    <MemoryRouter>
-      <Link to='/dashboard'>Dashboard Page</Link>
-    </MemoryRouter>,
-  );
+describe('Link component', () => {
+  it('should render styled link by default', () => {
+    render(
+      <MemoryRouter>
+        <Link to='/path/to/resource'>Dashboard Page</Link>
+      </MemoryRouter>,
+    );
 
-  const link = screen.getByRole('link', { name: /Dashboard Page/i });
+    const element = screen.getByRole('link', { name: /Dashboard Page/i });
 
-  expect(link).toHaveAttribute('href', '/dashboard');
-});
+    expect(element.tagName.toLowerCase()).toBe('a');
+    expect(element.getAttribute('class')).not.toBe('');
+    expect(element.getAttribute('href')).toBe('/path/to/resource');
+  });
 
-test('it should apply styled classes when styled prop is true', () => {
-  render(
-    <MemoryRouter>
-      <Link to='/'>Dashboard Page</Link>
-    </MemoryRouter>,
-  );
+  it('should render unstyled link when "unstyled" prop is true', () => {
+    render(
+      <MemoryRouter>
+        <Link to='/path/to/resource' unstyled>
+          Dashboard Page
+        </Link>
+      </MemoryRouter>,
+    );
 
-  const link = screen.getByRole('link', { name: /Dashboard Page/i });
+    const element = screen.getByRole('link', { name: /Dashboard Page/i });
 
-  expect(link).toHaveClass('text-brand');
-});
-
-test('it should not apply styled classes when styled prop is false', () => {
-  render(
-    <MemoryRouter>
-      <Link to='/' unstyled>
-        Dashboard Page
-      </Link>
-    </MemoryRouter>,
-  );
-
-  const link = screen.getByRole('link', { name: /Dashboard Page/i });
-
-  expect(link).not.toHaveClass('text-brand');
+    expect(element.getAttribute('class')).toBe('');
+  });
 });
